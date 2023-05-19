764bf091a2ccd9d5107f8791312e02cac144fcb9
Rohit Anish <rohitanish158@gmail.com>
2023-05-19 13:57:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Hero from './pages/home/Hero'
+import Login from './pages/login/Login';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
